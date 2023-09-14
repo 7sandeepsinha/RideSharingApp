6a605ee7e2e2d8d53cbc8fff98a68a7381eee6e3
sndpoffcl <sandeep2official@gmail.com>
2023-09-14 08:31:33 +0530

    Ride sharing app models

---
+++ src/entity/Passenger.java
@@ -0,0 +1,33 @@
+package entity;
+
+import java.util.List;
+
+public class Passenger extends User{
+    private List<Trip> bookedTrips;
+
+    public Passenger(int id, String name, int age, String phoneNumber, double rating, List<String> reviews, char gender, List<Trip> bookedTrips) {
+        super(id, name, age, phoneNumber, rating, reviews, gender);
+        this.bookedTrips = bookedTrips;
+    }
+
+    public Passenger() {
+    }
+
+    public Passenger(List<Trip> bookedTrips) {
+        this.bookedTrips = bookedTrips;
+    }
+
+    public List<Trip> getBookedTrips() {
+        return bookedTrips;
+    }
+
+    public void setBookedTrips(List<Trip> bookedTrips) {
+        this.bookedTrips = bookedTrips;
+    }
+}
+
+// QuickRide Code 1 -> complete business logic -> 18th
+//19th break
+// QuickRide Code 2 -> Sprint Boot and Unit Testing -> 21st
+
+//Project Module -> 25th
---
+++ src/entity/Vehicle.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Vehicle {
+    private int id;
+    private String name;
+    private String number;
+    private VehicleType type;
+    private String color;
+}
